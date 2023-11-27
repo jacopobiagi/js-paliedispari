d6928f98f84b577207b7cdab66fbd0b52b16bb04
jacopobiagi <jacopo.biagi02@gmail.com>
2023-11-27 20:12:03 +0100

    Ottimizzazione dei numeri random

--- esercizio-dispari-pari/script.js
+++ esercizio-dispari-pari/script.js
@@ -4,7 +4,7 @@ sub.addEventListener("click", confronto);
 
 function confronto(){
     let number = document.getElementById("number").value;
-    let numeroRandom = Math.floor(Math.random()*(5-1)+1);
+    let numeroRandom = Math.floor(Math.random()*(6-1)+1);
     let selectiont = document.getElementById("seleziona");
 
     if (((parseInt(number) + numeroRandom) % 2 == 0) && (parseInt(selectiont.value) == 0)) {
